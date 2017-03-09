8ad14c1e6d326116afe3e2e8b0872b97f5b3cfd8
Markus Opolka <markus.opolka@iis.fraunhofer.de>
2017-03-09 12:26:11 +0100

    Remove legacy repo from deployment

--- manifests/mitarbeiterlog.pp
+++ manifests/mitarbeiterlog.pp
@@ -1,14 +1,11 @@
-# Role for Mitarbeiterlog
+# Role for Legacy Mitarbeiterlog
 #
-class vision_roles::mitarbeiterlog(
+class vision_roles::mitarbeiterlog (
 
   String $mysql_user          = hiera('mitarbeiterlog::database::user'),
   String $mysql_pass          = hiera('mitarbeiterlog::database::password'),
   String $mysql_db            = hiera('mitarbeiterlog::database::name'),
-  String $deploy_key_public   = hiera('mitarbeiterlog::deploy::public'),
-  String $deploy_key_private  = hiera('mitarbeiterlog::deploy::private'),
   String $path                = hiera('mitarbeiterlog::rootdir'),
-  String $url                 = 'ssh://git@vision-ha.iis.fhg.de:2222/vision/mitarbeiterlog.git',
   String $username            = 'mbl'
 
 ) {
@@ -18,24 +15,11 @@ class vision_roles::mitarbeiterlog(
   contain ::vision_mysql::server
   contain ::vision_apache::server
 
-  ::vision_deployment::user { $username:
-    ssh_private_key => $deploy_key_private,
-    ssh_public_key  => $deploy_key_public
-  }->
   file { 'Mitarbeiterlog root':
     ensure => directory,
     path   => $path,
     owner  => $username,
     group  => $username,
-  }->
-  vcsrepo { $path:
-    ensure   => present,
-    provider => git,
-    source   => $url,
-    revision => 'master',
-    user     => $username,
-    force    => true,
-    require  => User[$username],
   }->
   file { "${path}/dbconfig.inc.php":
     ensure  => present,
