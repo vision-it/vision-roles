0cf3fab737dd052d4726536f59a62776abcbb0f3
Markus Opolka <markus.opolka@iis.fraunhofer.de>
2017-03-14 10:09:18 +0100

    Change order to ensure icingaweb2 variable are evaluated for grafana

--- manifests/monitoring.pp
+++ manifests/monitoring.pp
@@ -4,8 +4,8 @@ class vision_roles::monitoring {
 
   contain ::vision_apache::server
   contain ::vision_ca
-  contain ::vision_icinga2::grafana
   contain ::vision_icinga2::icingaweb2
+  contain ::vision_icinga2::grafana
   contain ::vision_icinga2::server::feature::graphite
 
   # Legacy
