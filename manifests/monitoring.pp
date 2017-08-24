1ef1a1c50438c52f4add2d18549a6a3d2be7e4e3
Markus Opolka <markus.opolka@iis.fraunhofer.de>
2017-08-24 13:13:10 +0200

    Remove legacy code

--- manifests/monitoring.pp
+++ manifests/monitoring.pp
@@ -5,8 +5,4 @@ class vision_roles::monitoring {
   contain ::vision_apache::server
   contain ::vision_icinga2::icingaweb2
 
-  # Commented since in development
-  # contain ::vision_icinga2::grafana
-  # contain ::vision_icinga2::server::feature::graphite
-
 }
