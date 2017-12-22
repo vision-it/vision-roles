42ef2fc4c8f74961aee97d6d50f07f971046d543
Markus Opolka <opolkams@iis.fraunhofer.de>
2017-12-22 15:30:23 +0100

    Prepare new mbl profile

--- manifests/mitarbeiterlog.pp
+++ manifests/mitarbeiterlog.pp
@@ -1,38 +1,9 @@
-# Role for Legacy Mitarbeiterlog
+# Role for Mitarbeiterlog
 #
-class vision_roles::mitarbeiterlog (
+class vision_roles::mitarbeiterlog {
 
-  String $mysql_user          = hiera('mitarbeiterlog::database::user'),
-  String $mysql_pass          = hiera('mitarbeiterlog::database::password'),
-  String $mysql_db            = hiera('mitarbeiterlog::database::name'),
-  String $path                = hiera('mitarbeiterlog::rootdir'),
-  String $username            = 'mbl'
-
-) {
-
-  contain ::vision_mysql::server
-  contain ::vision_apache::server
-
-  file { 'Mitarbeiterlog root':
-    ensure => directory,
-    path   => $path,
-    owner  => $username,
-    group  => $username,
-  }
-  # opolkams: vision modules was moved to legacy, hopefully we dont need to deploy this anymore
-  # -> file { "${path}/dbconfig.inc.php":
-  #   ensure  => present,
-  #   owner   => $username,
-  #   group   => $username,
-  #   mode    => '0660',
-  #   content => template('vision/mitarbeiterlog/dbconfig.inc.php.erb'),
-  # }
-
-  ::mysql::db { $mysql_db:
-    user     => $mysql_user,
-    password => $mysql_pass,
-    host     => '%',
-    grant    => ['SELECT', 'UPDATE', 'CREATE', 'INSERT', 'ALTER', 'DELETE', 'DROP'],
-  }
+  # Removed legacy stuff and added docker for preperation
+  # will be replaced by vision_mbl soon
+  contain ::vision_docker
 
 }
