ca2771a6afe04f0ac14a46ff1851eb480257e0d0
Markus Opolka <opolkams@iis.fraunhofer.de>
2017-12-28 14:31:09 +0100

    Add vision_mbl to mitarbeiterlog role

--- manifests/mitarbeiterlog.pp
+++ manifests/mitarbeiterlog.pp
@@ -2,8 +2,6 @@
 #
 class vision_roles::mitarbeiterlog {
 
-  # Removed legacy stuff and added docker for preperation
-  # will be replaced by vision_mbl soon
-  contain ::vision_docker
+  contain ::vision_mbl
 
 }
