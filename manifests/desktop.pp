7b32023b21cab9393cb3186f34462d92772268a1
Markus Opolka <opolkams@iis.fraunhofer.de>
2018-01-08 13:55:36 +0100

    Update Desktop

--- manifests/desktop.pp
+++ manifests/desktop.pp
@@ -5,9 +5,8 @@ class vision_roles::desktop {
   contain ::vision_cups
   contain ::vision_docker
   contain ::vision_editors::phpstorm
-  contain ::vision_kerberos::client
-  contain ::vision_ldap::client
-  contain ::vision_nfs::client
+  contain ::vision_idm
+  contain ::vision_nfs
   contain ::vision_nodejs
   contain ::vision_packer
 
