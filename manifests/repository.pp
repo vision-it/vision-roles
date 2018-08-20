2ee617d4fba1cf9b8f6093c1a516f0ed74c48d2d
Jack Henschel <jackdev@mailbox.org>
2018-08-20 09:44:25 +0200

    Remove obsolete puppet code
    webhookd and rabbitmq are currently not used

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -2,9 +2,6 @@
 #
 class vision_roles::repository (
 
-  String $rabbitmq_user     = hiera('rabbitmq_user'),
-  String $rabbitmq_password = hiera('rabbitmq_password'),
-
 ) {
 
   contain ::vision_aptutil::cacher
@@ -12,22 +9,5 @@ class vision_roles::repository (
   # contain ::vision_aptly
   contain ::vision_docker
   contain ::vision_registry
-  contain ::vision_webhookd
-
-  ::docker::image { 'rabbitmq':
-    ensure    => present,
-    image     => 'rabbitmq',
-    image_tag => '3.6.10',
-  }
-
-  ::docker::run { 'rabbitmq':
-    ensure => present,
-    image  => 'rabbitmq:3.6.10',
-    ports  => [ '5672:5672' ],
-    env    => [
-      "RABBITMQ_DEFAULT_USER=${rabbitmq_user}",
-      "RABBITMQ_DEFAULT_PASS=${rabbitmq_password}",
-    ],
-  }
 
 }
