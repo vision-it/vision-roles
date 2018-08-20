5c73dc89ad61f7bd3071d1d85f39a179d3fc6847
Jack Henschel <jackdev@mailbox.org>
2018-08-20 10:13:56 +0200

    Remove temporary exclusion of aptly module
    upstream issue has been fixed

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -5,8 +5,7 @@ class vision_roles::repository (
 ) {
 
   contain ::vision_aptutil::cacher
-  # Temporary https://github.com/gds-operations/puppet-aptly/pull/69
-  # contain ::vision_aptly
+  contain ::vision_aptly
   contain ::vision_docker
   contain ::vision_registry
 
