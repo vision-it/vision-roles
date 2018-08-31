f4c4d56f787de628c07bdec4ab92f29e5903841a
Jack Henschel <jackdev@mailbox.org>
2018-08-31 09:22:22 +0200

    Migrate proxy from apache to nginx on prx01.prd.dmz

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -2,7 +2,19 @@
 #
 class vision_roles::proxy {
 
-  contain ::vision_apache::proxy
-  contain ::vision_apache::ssl
+  # temporary during migration -- henschjk 2018-08-31
+  if $::fqdn == 'prx01.prd.int.vision.fhg.de' {
+    package { [
+      'apache2',
+      'apache2-bin',
+      'apache2-data',
+      'apache2-utils',
+    ]:
+      ensure => 'absent',
+    }
+    contain ::vision_nginx
+  } else {
+    contain ::vision_apache
+  }
 
 }
