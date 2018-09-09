06a13c5b6275a17afad99a38252095d1dbc9c528
Jack Henschel <jackdev@mailbox.org>
2018-09-09 19:52:28 +0200

    Switch to nginx proxy in production
    shipit!

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -3,18 +3,18 @@
 class vision_roles::proxy {
 
   # temporary during migration -- henschjk 2018-08-31
-  if $::fqdn == 'prx01.prd.int.vision.fhg.de' {
-    package { [
-      'apache2',
-      'apache2-bin',
-      'apache2-data',
-      'apache2-utils',
-    ]:
-      ensure => 'absent',
-    }
-    contain ::vision_nginx
-  } else {
-    contain ::vision_apache
+  package { [
+             'apache2',
+             'apache2-bin',
+             'apache2-data',
+             'apache2-utils',
+             ]:
+               ensure => 'absent',
   }
+  service { 'apache2':
+    ensure => 'stopped',
+  }
+
+  contain ::vision_nginx
 
 }
