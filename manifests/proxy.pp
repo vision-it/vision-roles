82962e9227a6a72c1300398976c97b59ee4553b6
Jack Henschel <jackdev@mailbox.org>
2018-09-09 20:18:27 +0200

    Fix two-space soft tabs
    -.-

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -4,12 +4,12 @@ class vision_roles::proxy {
 
   # temporary during migration -- henschjk 2018-08-31
   package { [
-             'apache2',
-             'apache2-bin',
-             'apache2-data',
-             'apache2-utils',
-             ]:
-               ensure => 'absent',
+    'apache2',
+    'apache2-bin',
+    'apache2-data',
+    'apache2-utils',
+    ]:
+      ensure => 'absent',
   }
   service { 'apache2':
     ensure => 'stopped',
