bb0c70226255f57fffa545f07ac739b48f8f0904
Jack Henschel <jackdev@mailbox.org>
2019-01-08 15:42:48 +0100

    Remove migration code from proxy role
    migration from apache to nginx successful

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -2,19 +2,6 @@
 #
 class vision_roles::proxy {
 
-  # temporary during migration -- henschjk 2018-08-31
-  package { [
-    'apache2',
-    'apache2-bin',
-    'apache2-data',
-    'apache2-utils',
-    ]:
-      ensure => 'absent',
-  }
-  service { 'apache2':
-    ensure => 'stopped',
-  }
-
   contain ::vision_nginx
 
 }
