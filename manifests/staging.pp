3cdb86738ca14da2e0b64db462d466a09a906b86
Jack Henschel <jackdev@mailbox.org>
2019-01-14 16:58:20 +0100

    Add mailhog to role staging
    fixes https://vision-it.iis.fhg.de/git/puppet/control/issues/133

--- manifests/staging.pp
+++ manifests/staging.pp
@@ -6,4 +6,18 @@ class vision_roles::staging {
   contain ::vision_webshop
   contain ::vision_ssh::client
 
+  ::docker::image { 'mailhog':
+    ensure => present,
+    image => 'mailhog/mailhog',
+  }
+
+  ::docker::run { 'mailhog-webshop':
+    image   => 'mailhog/mailhog',
+    ports   => [
+      "1025:1025",
+      "8025:8025",
+    ],
+    require => Docker::Image['mailhog'],
+  }
+
 }
