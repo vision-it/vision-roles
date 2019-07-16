e40c7a7de0ae5e833f31206283978ca185729aac
Jack Henschel <jackdev@mailbox.org>
2019-07-16 11:13:52 +0200

    Add Traefik and Minio docker swarm services to new cluster

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -7,4 +7,17 @@ class vision_roles::cluster {
   contain ::vision_docker::swarm
   contain ::vision_gluster::node
 
+  contain ::vision_traefik
+
+  $compose = [
+    '/vision/data/swarm/traefik.yaml',
+    '/vision/data/swarm/minio.yaml',
+  ]
+
+  docker_stack { 'vision':
+    ensure        => present,
+    compose_files => $compose,
+    require       => File[$compose],
+  }
+
 }
