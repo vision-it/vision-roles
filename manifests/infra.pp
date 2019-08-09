e90e50a7a88eb855d1182c1a3147578280492511
Markus Opolka <opolkams@iis.fraunhofer.de>
2019-08-09 10:10:56 +0200

    Temporarily disable icinga on infra role

--- manifests/infra.pp
+++ manifests/infra.pp
@@ -2,9 +2,10 @@
 #
 class vision_roles::infra {
 
-  contain ::vision_bareos
-  contain ::vision_icinga2::icingaweb2
-  contain ::vision_rsync
   contain ::vision_aptly
+  contain ::vision_bareos
+  contain ::vision_mysql
+  contain ::vision_rsync
+  # contain ::vision_icinga2::icingaweb2
 
 }
