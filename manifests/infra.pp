887317528a483e65f7f54ca38537368307636ab6
Markus Opolka <opolkams@iis.fraunhofer.de>
2019-08-09 13:54:57 +0200

    Enable icingaweb on infra

--- manifests/infra.pp
+++ manifests/infra.pp
@@ -6,6 +6,6 @@ class vision_roles::infra {
   contain ::vision_bareos
   contain ::vision_mysql::server
   contain ::vision_rsync
-  # contain ::vision_icinga2::icingaweb2
+  contain ::vision_icinga2::icingaweb2
 
 }
