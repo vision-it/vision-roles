1b372054b842437b8b0c918e89b975d8d2bef712
Markus Opolka <opolkams@iis.fraunhofer.de>
2019-08-09 11:50:56 +0200

    Fix profile name

--- manifests/infra.pp
+++ manifests/infra.pp
@@ -4,7 +4,7 @@ class vision_roles::infra {
 
   contain ::vision_aptly
   contain ::vision_bareos
-  contain ::vision_mysql
+  contain ::vision_mysql::server
   contain ::vision_rsync
   # contain ::vision_icinga2::icingaweb2
 
