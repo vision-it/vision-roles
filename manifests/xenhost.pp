5c9065500c44eca30c674f3f1a7f3751b6370d25
Markus Opolka <opolkams@iis.fraunhofer.de>
2019-08-21 19:40:01 +0200

    Revert "Add puppetserver config to xenhost"
    This reverts commit 7508151341559e76b9c4d3ea323d595d1c7e555f.

--- manifests/xenhost.pp
+++ manifests/xenhost.pp
@@ -4,9 +4,4 @@ class vision_roles::xenhost {
 
   contain ::vision_xen
 
-  # Until everything ist masterless, the xen host is the new master
-  contain ::vision_puppet::hiera
-  contain ::vision_puppet::r10k
-  contain ::vision_puppet::server
-
 }
