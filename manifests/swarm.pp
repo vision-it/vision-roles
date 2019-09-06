ded66b4d15765933071ce9c1d09f9aa04d787d8d
Jack Henschel <jackdev@mailbox.org>
2019-09-06 19:08:39 +0200

    Add new role "swarm" for internal "cluster"

---
+++ manifests/swarm.pp
@@ -0,0 +1,25 @@
+# Role for internal Swarm/MySQL "Cluster"
+#
+class vision_roles::swarm {
+
+  contain ::vision_mysql::mariadb
+  contain ::vision_docker
+  contain ::vision_docker::swarm
+
+  contain ::vision_traefik
+#  contain ::vision_webshop
+#  contain ::vision_intranet
+
+  $compose = [
+    '/vision/data/swarm/traefik.yaml',
+#    '/vision/data/swarm/webshop.yaml',
+#    '/vision/data/swarm/intranet.yaml',
+  ]
+
+  docker_stack { 'vision':
+    ensure        => present,
+    compose_files => $compose,
+    require       => File[$compose],
+  }
+
+}
