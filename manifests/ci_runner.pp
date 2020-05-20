4ce5dff0441f6dc7548a841a4fb21bdf8168de41
Markus Opolka <markus@martialblog.de>
2020-05-20 15:18:12 +0200

    Add logsync Profile

--- manifests/ci_runner.pp
+++ manifests/ci_runner.pp
@@ -4,6 +4,7 @@ class vision_roles::ci_runner {
 
   contain ::vision_docker
   contain ::vision_loki::server
+  contain ::vision_loki::logsync
   contain ::vision_loki::promtail
   contain ::gitlab_ci_runner
 
