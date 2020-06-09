71057cc7c6abe641f016dc661614497b698cc416
Markus Opolka <opolkams@iis.fraunhofer.de>
2020-06-09 12:17:46 +0200

    Merge infra and ci_runner role

--- manifests/infra.pp
+++ manifests/infra.pp
@@ -2,6 +2,10 @@
 #
 class vision_roles::infra {
 
+  contain ::vision_docker
+  contain ::vision_loki::server
+  contain ::vision_loki::logsync
+  contain ::vision_loki::promtail
   contain ::vision_prometheus
   contain ::vision_aptly
   contain ::vision_bareos
@@ -9,5 +13,6 @@ class vision_roles::infra {
   contain ::vision_rsync
   contain ::vision_icinga2::icingaweb2
   contain ::vision_loki::promtail
+  contain ::gitlab_ci_runner
 
 }
