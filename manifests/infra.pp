f286053031413378e7a25365499f10f8d7f71ede
Markus Opolka <markus@martialblog.de>
2020-06-26 13:31:02 +0200

    Remove rsync from infra

--- manifests/infra.pp
+++ manifests/infra.pp
@@ -10,7 +10,6 @@ class vision_roles::infra {
   contain ::vision_aptly
   contain ::vision_bareos
   contain ::vision_mysql::mariadb
-  contain ::vision_rsync
   contain ::vision_icinga2::icingaweb2
   contain ::vision_loki::promtail
   contain ::gitlab_ci_runner
